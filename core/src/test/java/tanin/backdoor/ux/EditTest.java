fa5d7d078f967ebdd67172ca7670b1a51b4c66ca
tanin47 <tanin47@users.noreply.github.com>
2026-01-22 18:38:00 +0700

    Support the 'now' button if the column is timestamp

--- core/src/test/java/tanin/backdoor/ux/EditTest.java
+++ core/src/test/java/tanin/backdoor/ux/EditTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.JavascriptExecutor;
 import tanin.backdoor.Base;
+import tanin.backdoor.core.engine.Engine;
+
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+import java.time.Instant;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,5 +34,44 @@ public class EditTest extends Base {
     fill(tid("new-value"), "new-password");
     click(tid("submit-button"));
     waitUntil(() -> assertEquals("new-password", elem(tid("sheet-column-value", "password")).getText().trim()));
+  }
+
+  @Test
+  void useNowTimestamp() throws Exception {
+    try (var engine = server.engineProvider.createEngine(postgresConfig, null)) {
+      engine.connection.createStatement().execute("""
+            CREATE TABLE "date_time" (
+              id INT PRIMARY KEY,
+              timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL
+            )
+        """);
+
+      engine.connection.createStatement().execute(
+        """
+          
+             INSERT INTO "date_time" (
+             id,
+             timestamp
+           ) VALUES (
+             '1',
+             TIMESTAMP '2025-10-19T10:00:01.000000Z' AT TIME ZONE 'UTC'
+           )
+          """
+      );
+    }
+    go("/");
+    click(tid("database-item"));
+    waitUntil(() -> assertEquals("loaded", elem(tid("database-item")).getDomAttribute("data-database-status")));
+
+    click(tid("menu-items", "postgres", null, "menu-item-table", "date_time"));
+
+    waitUntil(() -> assertTrue(hasElem(tid("sheet-tab", "date_time"))));
+    click(tid("sheet-column-value", "timestamp", null, "edit-field-button"));
+    click(tid("timestamp-now-button"));
+    click(tid("submit-button"));
+    waitUntil(() -> {
+      var timestamp = Instant.parse(elem(tid("sheet-column-value", "timestamp")).getText().trim());
+      assertTrue((Instant.now().getEpochSecond() - timestamp.getEpochSecond()) < 10);
+    });
   }
 }
